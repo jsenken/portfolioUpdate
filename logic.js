5208b44dc5de86e36b5e2ddc715cff35c2cead95
Jordan Senken <jsenken@yahoo.com>
2019-02-23 00:43:17 -0500

    overhaul

---
+++ logic.js
@@ -0,0 +1,48 @@
+function showPool(){
+    $(".filler").hide();
+    $("#ppFiller").show();
+}
+function showBlood(){
+    $(".filler").hide();
+    $("#bsFiller").show();
+}
+
+function showNews(){
+    $(".filler").hide();
+    $("#nsFiller").show();
+}
+
+function showGif(){
+    $(".filler").hide();
+    $("#gtFiller").show();
+}
+
+function showNBA(){
+    $(".filler").hide();
+    $("#naFiller").show();
+}
+
+function showBA(){
+    $(".filler").hide();
+    $("#baFiller").show();
+}
+
+function showBr(){
+    $(".filler").hide();
+    $("#brFiller").show();
+}
+
+function showTr(){
+    $(".filler").hide();
+    $("#trFiller").show();
+}
+
+function showHM(){
+    $(".filler").hide();
+    $("#dkFiller").show();
+}
+
+function showFF(){
+    $(".filler").hide();
+    $("#ffFiller").show();
+}
